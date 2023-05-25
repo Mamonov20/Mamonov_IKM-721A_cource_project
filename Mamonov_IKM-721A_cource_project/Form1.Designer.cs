4bc5d6e723fe4383023a97efc1eb4d11a4fe272b
Mamonov <mamonov0957@gmail.com>
2023-05-25 14:35:25 +0300

    Finish Task number 1

---
+++ Mamonov_IKM-721A_cource_project/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Mamonov_IKM_721A_cource_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(697, 548);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(149, 53);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(745, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Enabled = false;
+            this.tbInput.Location = new System.Drawing.Point(649, 446);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(254, 31);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1596, 914);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tClock;
+        private System.Windows.Forms.TextBox tbInput;
+    }
+}
+
